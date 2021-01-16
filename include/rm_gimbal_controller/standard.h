bd97e21d10cb58de7c2ef1ff1fded5e22bcd15ca
qiayuan <liaoqiayuan@gmail.com>
2021-01-16 17:58:16 +0800

    add class GimbalStandardController

--- include/rm_gimbal_controller/standard.h
+++ include/rm_gimbal_controller/standard.h
@@ -5,4 +5,30 @@
 #ifndef RM_GIMBAL_CONTROLLER_INCLUDE_STANDARD_H_
 #define RM_GIMBAL_CONTROLLER_INCLUDE_STANDARD_H_
 
+#include <control_toolbox/pid.h>
+#include <controller_interface/multi_interface_controller.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <rm_base/hardware_interface/robot_state_interface.h>
+#include <realtime_tools/realtime_publisher.h>
+#include <rm_msgs/GimbalCmd.h>
+
+namespace rm_gimbal_controller {
+
+class GimbalStandardController :
+    public controller_interface::MultiInterfaceController<
+        hardware_interface::EffortJointInterface, hardware_interface::RobotStateInterface> {
+ public:
+  GimbalStandardController() = default;
+  bool init(hardware_interface::RobotHW *robot_hw,
+            ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh) override;
+  void update(const ros::Time &time, const ros::Duration & /*period*/) override;
+ private:
+  control_toolbox::Pid pid_yaw_, pid_pitch_;
+  hardware_interface::JointHandle joint_yaw_, joint_pitch_;
+
+  ros::Subscriber sub_command_;
+  void commandCB(const rm_msgs::GimbalCmdConstPtr &msg);
+};
+}
+
 #endif //RM_GIMBAL_CONTROLLER_INCLUDE_STANDARD_H_
--- src/standard.cpp
+++ src/standard.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "rm_gimbal_controller/standard.h"
+#include <ros_utilities.h>
+
+namespace rm_gimbal_controller {
+bool GimbalStandardController::init(hardware_interface::RobotHW *robot_hw,
+                                    ros::NodeHandle &root_nh,
+                                    ros::NodeHandle &controller_nh) {
+  std::string joint_yaw_name = getParam(controller_nh, "joint_yaw_name", std::string("joint_yaw"));
+  std::string joint_pitch_name = getParam(controller_nh, "joint_pitch_name", std::string("joint_pitch"));
+
+  if (!pid_yaw_.init(ros::NodeHandle(controller_nh, "pid_yaw"))
+      || pid_pitch_.init(ros::NodeHandle(controller_nh, "pid_pitch")))
+    return false;
+
+  sub_command_ = root_nh.subscribe<rm_msgs::GimbalCmd>("cmd_gimbal", 1, &GimbalStandardController::commandCB, this);
+  return true;
+}
+
+void GimbalStandardController::update(const ros::Time &time, const ros::Duration &) {
+
+}
+
+void GimbalStandardController::commandCB(const rm_msgs::GimbalCmdConstPtr &msg) {
+
+}
+
+}
