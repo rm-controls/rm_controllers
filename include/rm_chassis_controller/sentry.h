b45171be512e595c51db23222ee425dfec073eb8
qiayuan <liaoqiayuan@gmail.com>
2021-02-09 21:03:00 +0800

    Move robot_state_controller to a single package

--- include/rm_chassis_controller/sentry.h
+++ include/rm_chassis_controller/sentry.h
@@ -8,8 +8,8 @@
 #include <control_toolbox/pid.h>
 #include <controller_interface/multi_interface_controller.h>
 #include <hardware_interface/joint_command_interface.h>
-#include <rm_base/hardware_interface/robot_state_interface.h>
-#include <rm_base/robot_state_controller/tf_rt_broadcaster.h>
+#include <robot_state_controller/robot_state_interface.h>
+#include <robot_state_controller/tf_rt_broadcaster.h>
 #include <realtime_tools/realtime_publisher.h>
 #include <rm_msgs/ChassisCmd.h>
 #include <filters.h>
@@ -33,7 +33,6 @@ class ChassisSentryController :
  private:
   void passive();
   void raw();
-  void tfVelFromYawToBase(const ros::Time &time);
   void recovery();
   void moveJoint(const ros::Duration &period);
   void commandCB(const rm_msgs::ChassisCmdConstPtr &msg);
@@ -43,11 +42,11 @@ class ChassisSentryController :
 
   control_toolbox::Pid pid_wheel_;
   hardware_interface::JointHandle joint_wheel_;
-  hardware_interface::RobotStateHandle robot_state_handle_;
+  hardware_interface::RobotStateHandle robot_state_handle_{};
 
   double publish_rate_{}, wheel_radius_{}, current_coeff_{};
   ros::Time last_publish_time_;
-  geometry_msgs::TransformStamped odom2base_;
+  geometry_msgs::TransformStamped odom2base_{};
   geometry_msgs::Vector3Stamped vel_cmd_{};
   geometry_msgs::Vector3Stamped vel_tfed_{};
 
@@ -60,7 +59,7 @@ class ChassisSentryController :
   realtime_tools::RealtimeBuffer<rm_msgs::ChassisCmd> chassis_rt_buffer_;
   realtime_tools::RealtimeBuffer<geometry_msgs::Twist> vel_rt_buffer_;
   rm_msgs::ChassisCmd cmd_chassis_;
-  robot_state_controller::TfRtBroadcaster tf_broadcaster_;
+  robot_state_controller::TfRtBroadcaster tf_broadcaster_{};
 };
 }
 #endif //RM_CHASSIS_CONTROLLER_STANDARD_H
--- include/rm_chassis_controller/standard.h
+++ include/rm_chassis_controller/standard.h
@@ -8,8 +8,8 @@
 #include <control_toolbox/pid.h>
 #include <controller_interface/multi_interface_controller.h>
 #include <hardware_interface/joint_command_interface.h>
-#include <rm_base/hardware_interface/robot_state_interface.h>
-#include <rm_base/robot_state_controller/tf_rt_broadcaster.h>
+#include <robot_state_controller/robot_state_interface.h>
+#include <robot_state_controller/tf_rt_broadcaster.h>
 #include <realtime_tools/realtime_publisher.h>
 #include <rm_msgs/ChassisCmd.h>
 #include <filters.h>
@@ -51,11 +51,11 @@ class ChassisStandardController :
   control_toolbox::Pid pid_rf_, pid_lf_, pid_rb_, pid_lb_;
   control_toolbox::Pid pid_follow_, pid_twist_;
   hardware_interface::JointHandle joint_rf_, joint_lf_, joint_rb_, joint_lb_;
-  hardware_interface::RobotStateHandle robot_state_handle_;
+  hardware_interface::RobotStateHandle robot_state_handle_{};
 
   double publish_rate_{}, wheel_base_{}, wheel_track_{}, wheel_radius_{}, current_coeff_{};
   ros::Time last_publish_time_;
-  geometry_msgs::TransformStamped odom2base_;
+  geometry_msgs::TransformStamped odom2base_{};
   geometry_msgs::Vector3Stamped vel_cmd_{};
   geometry_msgs::Vector3Stamped vel_tfed_{};
 
@@ -69,7 +69,7 @@ class ChassisStandardController :
   realtime_tools::RealtimeBuffer<geometry_msgs::Twist> vel_rt_buffer_;
   std::shared_ptr<realtime_tools::RealtimePublisher<nav_msgs::Odometry> > odom_pub_;
   rm_msgs::ChassisCmd cmd_chassis_;
-  robot_state_controller::TfRtBroadcaster tf_broadcaster_;
+  robot_state_controller::TfRtBroadcaster tf_broadcaster_{};
 };
 }
 #endif //RM_CHASSIS_CONTROLLER_STANDARD_H
