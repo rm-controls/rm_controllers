65e7e8fc17b6eb5b74dbba41f51d6597feab24bb
ChenWEIx2 <1374691864@qq.com>
2021-03-26 22:03:54 +0800

    Pass test_NAN_cmd

--- test/test_NAN_cmd.cpp
+++ test/test_NAN_cmd.cpp
@@ -16,39 +16,49 @@ TEST_F(StandardChassisTest, testCmdVelNaN) {
 
   cmd_chassis.mode = cmd_chassis.RAW;
   cmd_chassis.effort_limit = 99;
-  cmd_chassis.accel.linear.x = 0;
-  cmd_chassis.accel.linear.y = 0;
+  cmd_chassis.accel.linear.x = 8;
+  cmd_chassis.accel.linear.y = 8;
+  cmd_chassis.accel.angular.z = 8;
 
   publish(cmd_chassis, cmd_vel);
-  ros::Duration(0.1).sleep();
+  ros::Duration(2.0).sleep();
+
+  cmd_vel.linear.x = 1.0;
+  publish(cmd_chassis, cmd_vel);
+  ros::Duration(0.2).sleep();
+
 
   // send NaN
-  for (int i = 0; i < 10; ++i) {
-    cmd_vel.linear.x = NAN;
-    cmd_vel.linear.y = NAN;
-    cmd_vel.angular.z = NAN;
-    publish(cmd_chassis, cmd_vel);
-    geometry_msgs::Twist odom_msg = getTwist();
-    EXPECT_FALSE(std::isnan(odom_msg.linear.x));
-    EXPECT_FALSE(std::isnan(odom_msg.linear.y));
-    EXPECT_FALSE(std::isnan(odom_msg.angular.z));
-    ros::Duration(0.1).sleep();
-  }
-
+  cmd_vel.linear.x = NAN;
+  publish(cmd_chassis, cmd_vel);
+  ros::Duration(2.5).sleep();
   nav_msgs::Odometry odom = getLastOdom();
+
   EXPECT_NEAR(odom.twist.twist.linear.x, 0.0, VELOCITY_TOLERANCE);
+
+  cmd_vel.linear.x = 0;
+  cmd_vel.linear.y = 1.0;
+  publish(cmd_chassis, cmd_vel);
+  ros::Duration(0.2).sleep();
+
+  cmd_vel.linear.y = NAN;
+  publish(cmd_chassis, cmd_vel);
+  ros::Duration(2.0).sleep();
+  odom = getLastOdom();
+
   EXPECT_NEAR(odom.twist.twist.linear.y, 0.0, VELOCITY_TOLERANCE);
+
+  cmd_vel.linear.y = 0;
+  publish(cmd_chassis, cmd_vel);
+  ros::Duration(0.2).sleep();
+
+  cmd_vel.angular.z = NAN;
+  publish(cmd_chassis, cmd_vel);
+  ros::Duration(2.0).sleep();
+  odom = getLastOdom();
   EXPECT_NEAR(odom.twist.twist.angular.z, 0.0, VELOCITY_TOLERANCE);
-  EXPECT_NEAR(odom.pose.pose.position.x, 0.0, POSITION_TOLERANCE);
-  EXPECT_NEAR(odom.pose.pose.position.y, 0.0, POSITION_TOLERANCE);
-
-  geometry_msgs::Twist odom_msg = getTwist();
-  EXPECT_NEAR(odom_msg.linear.x, 0.0, VELOCITY_TOLERANCE);
-  EXPECT_NEAR(odom_msg.linear.y, 0.0, VELOCITY_TOLERANCE);
-  EXPECT_NEAR(odom_msg.angular.z, 0.0, VELOCITY_TOLERANCE);
-
-  ROS_INFO("Test send NAN cmd_vel success!");
 }
+
 
 //Test case2
 TEST_F(StandardChassisTest, testCmdChassisNaNEffortLimit) {
@@ -65,33 +75,27 @@ TEST_F(StandardChassisTest, testCmdChassisNaNEffortLimit) {
 
   cmd_chassis.mode = cmd_chassis.RAW;
   cmd_chassis.effort_limit = 99;
-  cmd_chassis.accel.linear.x = 0;
-  cmd_chassis.accel.linear.y = 0;
+  cmd_chassis.accel.linear.x = 8;
+  cmd_chassis.accel.linear.y = 8;
 
   publish(cmd_chassis, cmd_vel);
-  ros::Duration(0.1).sleep();
+  ros::Duration(2).sleep();
+
+  cmd_vel.linear.x = 1;
+  publish(cmd_chassis, cmd_vel);
+  ros::Duration(0.2).sleep();
 
   cmd_chassis.effort_limit = NAN;
   publish(cmd_chassis, cmd_vel);
-  ros::Duration(1).sleep();
-  cmd_vel.linear.x = 0.5;
-  publish(cmd_chassis, cmd_vel);
-
-  ros::Duration(10).sleep();
+  ros::Duration(2.5).sleep();
 
   nav_msgs::Odometry odom = getLastOdom();
   EXPECT_NEAR(odom.twist.twist.linear.x, 0.0, VELOCITY_TOLERANCE);
-  EXPECT_NEAR(odom.pose.pose.position.x, 0.0, POSITION_TOLERANCE);
-  EXPECT_NEAR(odom.pose.pose.position.y, 0.0, POSITION_TOLERANCE);
 
-  geometry_msgs::Twist odom_msg = getTwist();
-  EXPECT_NEAR(odom_msg.linear.x, 0.0, VELOCITY_TOLERANCE);
-
-  ROS_INFO("Test send NAN effort limit success!");
 }
 
+
 //Test case3
-//this case can't pass now
 TEST_F(StandardChassisTest, testCmdChassisNaNAccel) {
   // wait for ROS
   while (!isControllerAlive()) {
@@ -106,27 +110,48 @@ TEST_F(StandardChassisTest, testCmdChassisNaNAccel) {
 
   cmd_chassis.mode = cmd_chassis.RAW;
   cmd_chassis.effort_limit = 99;
-  cmd_chassis.accel.linear.x = 0;
-  cmd_chassis.accel.linear.y = 0;
+  cmd_chassis.accel.linear.x = 8;
+  cmd_chassis.accel.linear.y = 8;
+  cmd_chassis.accel.angular.z = 8;
 
   publish(cmd_chassis, cmd_vel);
-  ros::Duration(0.1).sleep();
+  ros::Duration(2.0).sleep();
 
-  cmd_vel.linear.x = 0.5;
+  cmd_vel.linear.x = 1.0;
+  publish(cmd_chassis, cmd_vel);
+  ros::Duration(0.2).sleep();
+
   cmd_chassis.accel.linear.x = NAN;
-  //cmd_chassis.accel.angular.z = NAN;
   publish(cmd_chassis, cmd_vel);
-  ros::Duration(5).sleep();
+  ros::Duration(2.5).sleep();
 
   nav_msgs::Odometry odom = getLastOdom();
   EXPECT_NEAR(odom.twist.twist.linear.x, 0.0, VELOCITY_TOLERANCE);
-  EXPECT_NEAR(odom.pose.pose.position.x, 0.0, POSITION_TOLERANCE);
-  EXPECT_NEAR(odom.pose.pose.position.y, 0.0, POSITION_TOLERANCE);
 
-  geometry_msgs::Twist odom_msg = getTwist();
-  EXPECT_NEAR(odom_msg.linear.x, 0.0, VELOCITY_TOLERANCE);
+  cmd_chassis.accel.linear.x = 0;
+  cmd_vel.linear.y = 1.0;
+  publish(cmd_chassis, cmd_vel);
+  ros::Duration(0.2).sleep();
 
-  ROS_INFO("Test send NAN accel linear x success!");
+  cmd_chassis.accel.linear.y = NAN;
+  publish(cmd_chassis, cmd_vel);
+  ros::Duration(2.0).sleep();
+
+  odom = getLastOdom();
+  EXPECT_NEAR(odom.twist.twist.linear.y, 0.0, VELOCITY_TOLERANCE);
+
+  cmd_chassis.accel.linear.y = 0;
+  cmd_vel.angular.z = 1.0;
+  publish(cmd_chassis, cmd_vel);
+  ros::Duration(0.2).sleep();
+
+  cmd_chassis.accel.angular.z = NAN;
+  publish(cmd_chassis, cmd_vel);
+  ros::Duration(2.0).sleep();
+
+  odom = getLastOdom();
+  EXPECT_NEAR(odom.twist.twist.angular.z, 0.0, VELOCITY_TOLERANCE);
+
 }
 
 int main(int argc, char **argv) {
