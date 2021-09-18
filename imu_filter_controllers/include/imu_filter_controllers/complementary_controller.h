0b1b3d68ada29af9c24a2c287ed6cb35a1a9dd0d
BruceLannn <1656257449@qq.com>
2021-09-18 23:28:23 +0800

    Complete complementary controller.

--- imu_filter_controllers/include/imu_filter_controllers/complementary_controller.h
+++ imu_filter_controllers/include/imu_filter_controllers/complementary_controller.h
@@ -1,68 +1,47 @@
-
-#ifndef IMU_TOOLS_COMPLEMENTARY_FILTER_ROS_H
-#define IMU_TOOLS_COMPLEMENTARY_FILTER_ROS_H
-
-#include <sensor_msgs/MagneticField.h>
-#include <geometry_msgs/Vector3Stamped.h>
-#include <message_filters/subscriber.h>
-#include <message_filters/sync_policies/approximate_time.h>
-#include <message_filters/synchronizer.h>
 #include <ros/ros.h>
+#include <controller_interface/multi_interface_controller.h>
+#include <controller_interface/controller.h>
 #include <sensor_msgs/Imu.h>
+#include <sensor_msgs/Temperature.h>
+#include <sensor_msgs/TimeReference.h>
+#include <realtime_tools/realtime_publisher.h>
 #include <tf/transform_datatypes.h>
-#include <tf/transform_broadcaster.h>
-
-#include "imu_complementary_filter/complementary_filter.h"
-#include <rm_common/hardware_interface/complementary_interface.h>
+#include <hardware_interface/imu_sensor_interface.h>
+#include <rm_common/hardware_interface/imu_extra_interface.h>
+#include <imu_complementary_filter/complementary_filter.h>
 
 namespace imu_filter_controllers
 {
 class ComplementaryController
+  : public controller_interface::MultiInterfaceController<hardware_interface::ImuSensorInterface,
+                                                          rm_control::ImuExtraInterface>
 {
 public:
   ComplementaryController() = default;
-  bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh);
-  void update(const ros::Time& time, const ros::Duration& /*period*/);
+  bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh) override;
+  void update(const ros::Time& time, const ros::Duration& /*period*/) override;
 
 private:
-  // ROS-related variables.
-  ros::NodeHandle nh_;
-  ros::NodeHandle nh_private_;
-
-  tf::TransformBroadcaster tf_broadcaster_;
-
-  // hardware_interface
-  hardware_interface::ComplementaryHandle complementary_handle_{};
-
   // Parameters:
   bool use_mag_;
-  bool publish_tf_;
-  bool reverse_tf_;
-  double constant_dt_;
   bool publish_debug_topics_;
-  std::string fixed_frame_;
 
-  // msg
-  geometry_msgs::Vector3 imu_angular_velocity_;
-  geometry_msgs::Vector3 imu_linear_acceleration_;
-  ros::Time imu_time_;
-  std::string imu_frame_id_;
+  // hardware_interface
+  hardware_interface::ImuSensorHandle imu_sensor_handle_;
+  rm_control::ImuExtraHandle imu_extra_handle_;
+  //
+  sensor_msgs::Temperature imu_temp_;
+  sensor_msgs::TimeReference trigger_time_;
+  std::string frame_id;
+
+  std::shared_ptr<realtime_tools::RealtimePublisher<sensor_msgs::Imu> > imu_data_pub_;
+  std::shared_ptr<realtime_tools::RealtimePublisher<sensor_msgs::Temperature> > imu_temp_pub_;
+  std::shared_ptr<realtime_tools::RealtimePublisher<sensor_msgs::TimeReference> > trigger_time_pub_;
 
   // State variables:
   imu_tools::ComplementaryFilter filter_;
-  ros::Time time_prev_;
+  ros::Time last_publish_time_;
   bool initialized_filter_;
-
-  /** @brief init params
-   *
-   */
-  void initializeParams();
-  void UpdateFilter(const geometry_msgs::Vector3 imu_angular_velocity_,
-                    const geometry_msgs::Vector3 imu_linear_acceleration_, const ros::Time time);
-
-  tf::Quaternion hamiltonToTFQuaternion(double q0, double q1, double q2, double q3) const;
 };
-
 }  // namespace imu_filter_controllers
 
-#endif  // IMU_TOOLS_COMPLEMENTARY_FILTER_ROS_H
--- imu_filter_controllers/src/complementary_controller.cpp
+++ imu_filter_controllers/src/complementary_controller.cpp
@@ -1,188 +1,38 @@
-//
-// Created by ljq on 2021/9/4.
-//
-
 #include "imu_filter_controllers/complementary_controller.h"
 
-#include <std_msgs/Float64.h>
-#include <std_msgs/Bool.h>
+#include <pluginlib/class_list_macros.hpp>
 
 namespace imu_filter_controllers
 {
 bool ComplementaryController::init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& root_nh,
                                    ros::NodeHandle& controller_nh)
 {
-  // base init
-  nh_ = controller_nh;
-  nh_private_ = root_nh;
   initialized_filter_ = false;
-
-  ROS_INFO("Starting ComplementaryFilterROS");
-  initializeParams();
-}
-
-void ComplementaryController::update(const ros::Time& time, const ros::Duration& /*period*/)
-{
-  // get new data from interface
-  imu_angular_velocity_ = complementary_handle_.getAngularVelocity();
-  imu_linear_acceleration_ = complementary_handle_.getLinearAcceleration();
-  imu_time_ = complementary_handle_.getStamp();
-  imu_frame_id_ = complementary_handle_.getFrameId();
-
-  double q0, q1, q2, q3;
-  filter_.getOrientation(q0, q1, q2, q3);
-  tf::Quaternion q = hamiltonToTFQuaternion(q0, q1, q2, q3);
-
-  // update filter
-  UpdateFilter(imu_angular_velocity_, imu_linear_acceleration_, imu_time_);
-
-  // calcute error and send data
-
-  // Account for biases.
-  if (filter_.getDoBiasEstimation())
-  {
-    imu_angular_velocity_.x -= filter_.getAngularVelocityBiasX();
-    imu_angular_velocity_.y -= filter_.getAngularVelocityBiasY();
-    imu_angular_velocity_.z -= filter_.getAngularVelocityBiasZ();
-  }
-
-  complementary_handle_.setAngularVelocity(imu_angular_velocity_);
-
-  /*
-  if (publish_debug_topics_)
-  {
-    // Create and publish roll, pitch, yaw angles
-    geometry_msgs::Vector3Stamped rpy;
-    rpy.header = imu_msg_raw->header;
-
-    tf::Matrix3x3 M;
-    M.setRotation(q);
-    M.getRPY(rpy.vector.x, rpy.vector.y, rpy.vector.z);
-    rpy_publisher_.publish(rpy);
-
-    // Publish whether we are in the steady state, when doing bias estimation
-    if (filter_.getDoBiasEstimation())
-    {
-      std_msgs::Bool state_msg;
-      state_msg.data = filter_.getSteadyState();
-      state_publisher_.publish(state_msg);
-    }
-  }
-   */
-
-  if (publish_tf_)
-  {
-    // Create and publish the ROS tf.
-    tf::Transform transform;
-    transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
-    transform.setRotation(q);
-
-    if (reverse_tf_)
-    {
-      tf_broadcaster_.sendTransform(tf::StampedTransform(transform.inverse(), imu_time_, imu_frame_id_, fixed_frame_));
-    }
-    else
-    {
-      tf_broadcaster_.sendTransform(tf::StampedTransform(transform, imu_time_, fixed_frame_, imu_frame_id_));
-    }
-  }
-}
-
-void ComplementaryController::UpdateFilter(const geometry_msgs::Vector3 imu_angular_velocity_,
-                                           const geometry_msgs::Vector3 imu_linear_acceleration_, const ros::Time time)
-{
-  const geometry_msgs::Vector3& a = imu_angular_velocity_;
-  const geometry_msgs::Vector3& w = imu_linear_acceleration_;
-
-  // Initialize.
-  if (!initialized_filter_)
-  {
-    time_prev_ = time;
-    initialized_filter_ = true;
-    return;
-  }
-
-  // determine dt: either constant, or from IMU timestamp
-  double dt;
-  if (constant_dt_ > 0.0)
-    dt = constant_dt_;
-  else
-    dt = (time - time_prev_).toSec();
-
-  time_prev_ = time;
-
-  // Update the filter.
-  filter_.update(a.x, a.y, a.z, w.x, w.y, w.z, dt);
-}
-
-/*
-void ComplementaryController::UpdateMagFilter(const geometry_msgs::Vector3 imu_angular_velocity_,
-                                              const geometry_msgs::Vector3 imu_linear_acceleration_,
-                                              const geometry_msgs::Vector3 m)
-{
-  const geometry_msgs::Vector3& a = imu_angular_velocity_;
-  const geometry_msgs::Vector3& w = imu_linear_acceleration_;
-
-  // Initialize.
-  if (!initialized_filter_)
-  {
-    time_prev_ = time;
-    initialized_filter_ = true;
-    return;
-  }
-
-  // determine dt: either constant, or from IMU timestamp
-  double dt;
-  if (constant_dt_ > 0.0)
-    dt = constant_dt_;
-  else
-    dt = (time - time_prev_).toSec();
-
-  time_prev_ = time;
-
-  // Update the filter.
-  filter_.update(a.x, a.y, a.z, w.x, w.y, w.z, dt);
-}
- */
-
-// send date
-
-tf::Quaternion ComplementaryController::hamiltonToTFQuaternion(double q0, double q1, double q2, double q3) const
-{
-  // ROS uses the Hamilton quaternion convention (q0 is the scalar). However,
-  // the ROS quaternion is in the form [x, y, z, w], with w as the scalar.
-  return tf::Quaternion(q1, q2, q3, q0);
-}
-
-void ComplementaryController::initializeParams()
-{
+  std::string name;
   double gain_acc;
   double gain_mag;
   bool do_bias_estimation;
   double bias_alpha;
   bool do_adaptive_gain;
 
-  if (!nh_private_.getParam("fixed_frame", fixed_frame_))
-    fixed_frame_ = "odom";
-  if (!nh_private_.getParam("use_mag", use_mag_))
+  if (!controller_nh.getParam("name", name))
+  {
+    ROS_ERROR("Name was not specified (namespace: %s)", controller_nh.getNamespace().c_str());
+    return false;
+  }
+  if (!controller_nh.getParam("use_mag", use_mag_))
     use_mag_ = false;
-  if (!nh_private_.getParam("publish_tf", publish_tf_))
-    publish_tf_ = false;
-  if (!nh_private_.getParam("reverse_tf", reverse_tf_))
-    reverse_tf_ = false;
-  if (!nh_private_.getParam("constant_dt", constant_dt_))
-    constant_dt_ = 0.0;
-  if (!nh_private_.getParam("publish_debug_topics", publish_debug_topics_))
+  if (!controller_nh.getParam("publish_debug_topics", publish_debug_topics_))
     publish_debug_topics_ = false;
-  if (!nh_private_.getParam("gain_acc", gain_acc))
+  if (!controller_nh.getParam("gain_acc", gain_acc))
     gain_acc = 0.01;
-  if (!nh_private_.getParam("gain_mag", gain_mag))
+  if (!controller_nh.getParam("gain_mag", gain_mag))
     gain_mag = 0.01;
-  if (!nh_private_.getParam("do_bias_estimation", do_bias_estimation))
+  if (!controller_nh.getParam("do_bias_estimation", do_bias_estimation))
     do_bias_estimation = true;
-  if (!nh_private_.getParam("bias_alpha", bias_alpha))
+  if (!controller_nh.getParam("bias_alpha", bias_alpha))
     bias_alpha = 0.01;
-  if (!nh_private_.getParam("do_adaptive_gain", do_adaptive_gain))
+  if (!controller_nh.getParam("do_adaptive_gain", do_adaptive_gain))
     do_adaptive_gain = true;
 
   filter_.setDoBiasEstimation(do_bias_estimation);
@@ -200,15 +50,59 @@ void ComplementaryController::initializeParams()
     if (!filter_.setBiasAlpha(bias_alpha))
       ROS_WARN("Invalid bias_alpha passed to ComplementaryFilter.");
   }
+  imu_sensor_handle_ = robot_hw->get<hardware_interface::ImuSensorInterface>()->getHandle(name);
+  imu_extra_handle_ = robot_hw->get<rm_control::ImuExtraInterface>()->getHandle(name);
 
-  // check for illegal constant_dt values
-  if (constant_dt_ < 0.0)
+  imu_data_pub_.reset(new realtime_tools::RealtimePublisher<sensor_msgs::Imu>(controller_nh, "imu_data", 100));
+  imu_temp_pub_.reset(
+      new realtime_tools::RealtimePublisher<sensor_msgs::Temperature>(controller_nh, "imu_temperature", 100));
+  trigger_time_pub_.reset(
+      new realtime_tools::RealtimePublisher<sensor_msgs::TimeReference>(controller_nh, "imu_trigger_time", 100));
+
+  return true;
+}
+
+void ComplementaryController::update(const ros::Time& time, const ros::Duration& period)
+{
+  if (!imu_extra_handle_.getGyroUpdated() || !imu_extra_handle_.getAccelUpdated())
+    return;
+  // Initialize.
+  if (!initialized_filter_)
   {
-    // if constant_dt_ is 0.0 (default), use IMU timestamp to determine dt
-    // otherwise, it will be constant
-    ROS_WARN("constant_dt parameter is %f, must be >= 0.0. Setting to 0.0", constant_dt_);
-    constant_dt_ = 0.0;
+    initialized_filter_ = true;
+    imu_data_pub_->msg_.header.frame_id = imu_sensor_handle_.getFrameId();
+    return;
+  }
+  const double* a = imu_sensor_handle_.getAngularVelocity();
+  const double* w = imu_sensor_handle_.getLinearAcceleration();
+
+  // Update the filter.
+  filter_.update(a[0], a[1], a[2], w[0], w[1], w[2], period.toSec());
+
+  double q0, q1, q2, q3;
+  filter_.getOrientation(q0, q1, q2, q3);
+  // ROS uses the Hamilton quaternion convention (q0 is the scalar). However,
+  // the ROS quaternion is in the form [x, y, z, w], with w as the scalar.
+  tf::Quaternion q(q1, q2, q3, q0);
+  geometry_msgs::Quaternion quat;
+  tf::quaternionTFToMsg(q, quat);
+  if (imu_extra_handle_.getCameraTrigger())
+  {
+    if (imu_data_pub_->trylock())
+    {
+      imu_data_pub_->msg_.header.stamp = time;
+      imu_data_pub_->msg_.orientation.x = quat.x;
+      imu_data_pub_->msg_.orientation.y = quat.y;
+      imu_data_pub_->msg_.orientation.z = quat.z;
+      imu_data_pub_->msg_.orientation.w = quat.w;
+      imu_temp_pub_->msg_.temperature = imu_extra_handle_.getTemperature();
+      trigger_time_pub_->msg_.time_ref = time;
+      imu_data_pub_->unlockAndPublish();
+    }
+    last_publish_time_ = time;
   }
 }
 
 }  // namespace imu_filter_controllers
+
+PLUGINLIB_EXPORT_CLASS(imu_filter_controllers::ComplementaryController, controller_interface::ControllerBase)
