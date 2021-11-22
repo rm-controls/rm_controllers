8aa8848185e7d4cb85b5ff2f359781e4eadb2784
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2021-11-22 22:30:08 +0800

    Update package.sh

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -3,7 +3,7 @@ ls
 source /opt/ros/noetic/setup.bash
 sudo apt-get install python3-bloom fakeroot dh-make devscripts
 pip install shyaml
-package_version=`curl -sL https://github.com/ros/rosdistro/raw/master/noetic/distribution.yaml | shyaml get-value repositories.rm_control.release.version`
+package_version=`curl -sL https://github.com/ros/rosdistro/raw/master/noetic/distribution.yaml | shyaml get-value repositories.rm_controllers.release.version`
 time_stamp=`date +%Y%m%d.%H%M%S`
 rosdep update
 for file in imu_filter_controllers rm_calibration_controllers rm_chassis_controllers rm_controllers rm_gimbal_controllers rm_orientation_controller rm_shooter_controllers robot_state_controller
