d6643713ffabac48475de1e71dacdbe944a33d7a
Edwinlinks <1026085014@qq.com>
2022-04-04 14:12:16 +0800

    Modify tof_sensor_controller reference to imu_sensor_controller

--- tof_sensor_controller/src/tof_sensor_controller.cpp
+++ tof_sensor_controller/src/tof_sensor_controller.cpp
@@ -7,29 +7,37 @@
 
 namespace tof_sensor_controller
 {
-bool Controller::init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh)
+bool Controller::init(rm_control::TofSensorInterface* hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh)
 {
-  std::string name;
-  if (!controller_nh.getParam("name", name))
-  {
-    ROS_ERROR("Name was not specified (namespace: %s)", controller_nh.getNamespace().c_str());
-    return false;
-  }
-  tof_sensor_handle = robot_hw->get<rm_control::TofSensorInterface>()->getHandle(name);
+  const std::vector<std::string>& tof_sensor_names = hw->getNames();
+  for (unsigned i = 0; i < tof_sensor_names.size(); i++)
+    ROS_DEBUG("Got sensor %s", tof_sensor_names[i].c_str());
 
-  tof_pub_.reset(new realtime_tools::RealtimePublisher<rm_msgs::TofSensor>(controller_nh, name + "/data", 100));
+  for (unsigned i = 0; i < tof_sensor_names.size(); i++)
+  {
+    // sensor handle
+    tof_sensors_.push_back(hw->getHandle(tof_sensor_names[i]));
+
+    // realtime publisher
+    tof_pub_.reset(
+        new realtime_tools::RealtimePublisher<rm_msgs::TofSensor>(controller_nh, tof_sensor_names[i] + "/data", 100));
+    realtime_pubs_.push_back(tof_pub_);
+  }
   return true;
 }
 
 void Controller::update(const ros::Time& time, const ros::Duration& period)
 {
-  if (tof_pub_->trylock())
+  for (unsigned int i = 0; i < realtime_pubs_.size(); ++i)
   {
-    tof_pub_->msg_.distance = tof_sensor_handle.getDistance();
-    tof_pub_->msg_.signal_strength = tof_sensor_handle.getStrength();
-    tof_pub_->msg_.dis_status = tof_sensor_handle.getStatus();
-    tof_pub_->msg_.stamp = time;
-    tof_pub_->unlockAndPublish();
+    if (realtime_pubs_[i]->trylock())
+    {
+      realtime_pubs_[i]->msg_.distance = tof_sensors_[i].getDistance();
+      realtime_pubs_[i]->msg_.signal_strength = tof_sensors_[i].getStrength();
+      realtime_pubs_[i]->msg_.dis_status = tof_sensors_[i].getStatus();
+      realtime_pubs_[i]->msg_.stamp = time;
+      realtime_pubs_[i]->unlockAndPublish();
+    }
   }
 }
 
