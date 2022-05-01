da4323518ce878c2e01195a6d71addcbe8ac837b
ljq-lv <939468378@qq.com>
2022-05-02 05:06:41 +0800

    Add the yaml of joint_mime_controller and class

---
+++ joint_mime_controller/include/joint_mime_controller/joint_mime_controller.h
@@ -0,0 +1,36 @@
+//
+// Created by ljq on 2022/5/1.
+//
+
+#pragma  once
+
+#include <rm_common/hardware_interface/robot_state_interface.h>
+#include <rm_common/tf_rt_broadcaster.h>
+
+#include <controller_interface/multi_interface_controller.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <pluginlib/class_list_macros.hpp>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/buffer.h>
+#include <urdf/model.h>
+#include <kdl/tree.hpp>
+#include <realtime_tools/realtime_buffer.h>
+
+namespace joint_mime_controller
+{
+class JointMimeController
+{
+public:
+		JointMimeController() = default;
+		bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& controller_nh);
+		void upddate(const ros::Time& time);
+
+private:
+
+};
+
+
+}
+
+PLUGINLIB_EXPORT_CLASS(joint_mime_controller::JointMimeController, controller_interface::ControllerBase)
---
+++ joint_mime_controller/src/joint_mime_controller.cpp
@@ -0,0 +1,20 @@
+//
+// Created by ljq on 2022/5/1.
+//
+
+#include "joint_mime_controller/joint_mime_controller.h"
+
+#include <kdl_parser/kdl_parser.hpp>
+#include <tf2_kdl/tf2_kdl.h>
+namespace joint_mime_controller
+{
+bool JointMimeController::init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& controller_nh)
+{
+
+};
+
+
+void JointMimeController::upddate(const ros::Time &time) {};
+
+
+}
