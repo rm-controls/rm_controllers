30fafc3075d133ce115dbc2ea99f3d4d57a8e284
LSY <2028238109@qq.com>
2023-03-16 21:38:44 +0800

    Fix cmakelist and pakeage.

--- multi_dof_controller/include/multi_dof_controller.h
+++ multi_dof_controller/include/multi_dof_controller.h
@@ -17,55 +17,56 @@ namespace multi_dof_controller
 {
 struct Joint
 {
-    std::string joint_name_;
-    effort_controllers::JointPositionController* ctrl_position_;
-    effort_controllers::JointVelocityController* ctrl_velocity_;
+  std::string joint_name_;
+  effort_controllers::JointPositionController* ctrl_position_;
+  effort_controllers::JointVelocityController* ctrl_velocity_;
 };
 struct Motion
 {
-    std::string motion_name_;
-    double position_per_step_;
-    double velocity_max_speed_;
-    std::vector<double> position_config_;
-    std::vector<double> velocity_config_;
-    std::vector<bool> is_position_need_reverse_;
-    std::vector<bool> is_velocity_need_reverse_;
+  std::string motion_name_;
+  double position_per_step_;
+  double velocity_max_speed_;
+  std::vector<double> position_config_;
+  std::vector<double> velocity_config_;
+  std::vector<bool> is_position_need_reverse_;
+  std::vector<bool> is_velocity_need_reverse_;
 };
 
-class Controller : public  controller_interface::MultiInterfaceController<rm_control::RobotStateInterface,hardware_interface::EffortJointInterface>
+class Controller : public controller_interface::MultiInterfaceController<rm_control::RobotStateInterface,
+                                                                         hardware_interface::EffortJointInterface>
 {
 public:
-    Controller() = default;
-    bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh) override;
-    void starting(const ros::Time& time) override;
-    void update(const ros::Time& time, const ros::Duration& period) override;
+  Controller() = default;
+  bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh) override;
+  void starting(const ros::Time& time) override;
+  void update(const ros::Time& time, const ros::Duration& period) override;
 
 private:
-    void position(const ros::Time& time,const ros::Duration& period);
-    void velocity(const ros::Time& time,const ros::Duration& period);
-    void commandCB(const rm_msgs::MultiDofCmdPtr& msg);
-    double getDirectionValue();
-    double judgeReverse(double value,bool is_need_reverse);
-    rm_control::RobotStateHandle robot_state_handle_;
-    effort_controllers::JointPositionController ctrl_yaw_, ctrl_pitch_;
+  void position(const ros::Time& time, const ros::Duration& period);
+  void velocity(const ros::Time& time, const ros::Duration& period);
+  void commandCB(const rm_msgs::MultiDofCmdPtr& msg);
+  double getDirectionValue();
+  double judgeReverse(double value, bool is_need_reverse);
+  rm_control::RobotStateHandle robot_state_handle_;
+  effort_controllers::JointPositionController ctrl_yaw_, ctrl_pitch_;
 
-    // ROS Interface
-    ros::Subscriber cmd_multi_dof_sub_;
-    realtime_tools::RealtimeBuffer<rm_msgs::MultiDofCmd> cmd_rt_buffer_;
-    hardware_interface::EffortJointInterface* effort_joint_interface_{};
-    std::vector<hardware_interface::JointHandle> joint_handles_{};
+  // ROS Interface
+  ros::Subscriber cmd_multi_dof_sub_;
+  realtime_tools::RealtimeBuffer<rm_msgs::MultiDofCmd> cmd_rt_buffer_;
+  hardware_interface::EffortJointInterface* effort_joint_interface_{};
+  std::vector<hardware_interface::JointHandle> joint_handles_{};
 
-    rm_msgs::MultiDofCmd cmd_multi_dof_;
-    bool state_changed_{};
+  rm_msgs::MultiDofCmd cmd_multi_dof_;
+  bool state_changed_{};
 
-    enum
-    {
-        VELOCITY,
-        POSITION
-    };
-    int state_ = VELOCITY;
-    std::vector<Joint> joints_;
-    std::vector<Motion> motions_;
+  enum
+  {
+    VELOCITY,
+    POSITION
+  };
+  int state_ = VELOCITY;
+  std::vector<Joint> joints_;
+  std::vector<Motion> motions_;
 };
 
-}// namespace multi_dof_controller
+}  // namespace multi_dof_controller
--- multi_dof_controller/src/multi_dof_controller.cpp
+++ multi_dof_controller/src/multi_dof_controller.cpp
@@ -9,153 +9,157 @@
 #include <rm_common/ros_utilities.h>
 #include <pluginlib/class_list_macros.hpp>
 
-
 namespace multi_dof_controller
 {
 bool Controller::init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh)
 {
-    XmlRpc::XmlRpcValue joints,motions;
-    controller_nh.getParam("joints", joints);
-    controller_nh.getParam("motions", motions);
-    ROS_ASSERT(joints.getType() == XmlRpc::XmlRpcValue::TypeStruct);
-    effort_joint_interface_ = robot_hw->get<hardware_interface::EffortJointInterface>();
-    for (const auto& joint : joints)
+  XmlRpc::XmlRpcValue joints, motions;
+  controller_nh.getParam("joints", joints);
+  controller_nh.getParam("motions", motions);
+  ROS_ASSERT(joints.getType() == XmlRpc::XmlRpcValue::TypeStruct);
+  effort_joint_interface_ = robot_hw->get<hardware_interface::EffortJointInterface>();
+  for (const auto& joint : joints)
+  {
+    Joint j{ .joint_name_ = joint.first,
+             .ctrl_position_ = new effort_controllers::JointPositionController(),
+             .ctrl_velocity_ = new effort_controllers::JointVelocityController() };
+    ros::NodeHandle nh_position = ros::NodeHandle(controller_nh, "joints/" + joint.first + "position");
+    ros::NodeHandle nh_velocity = ros::NodeHandle(controller_nh, "joints/" + joint.first + "velocity");
+    if (!j.ctrl_position_->init(effort_joint_interface_, nh_position) ||
+        !j.ctrl_velocity_->init(effort_joint_interface_, nh_velocity))
+      return false;
+    joint_handles_.push_back(j.ctrl_position_->joint_);
+    joint_handles_.push_back(j.ctrl_velocity_->joint_);
+    joints_.push_back(j);
+  }
+  for (const auto& motion : motions)
+  {
+    Motion m{ .motion_name_ = motion.first,
+              .position_per_step_ = motion.second["position_per_step"],
+              .velocity_max_speed_ = motion.second["velocity_max_speed"] };
+    for (int i = 0; i < motion.second["position_config"].size(); ++i)
     {
-        Joint j{ .joint_name_ = joint.first,
-                .ctrl_position_ = new effort_controllers::JointPositionController(),
-                .ctrl_velocity_ = new effort_controllers::JointVelocityController() };
-        ros::NodeHandle nh_position = ros::NodeHandle(controller_nh, "joints/" + joint.first + "position");
-        ros::NodeHandle nh_velocity = ros::NodeHandle(controller_nh, "joints/" + joint.first + "velocity");
-        if (!j.ctrl_position_->init(effort_joint_interface_, nh_position) ||
-            !j.ctrl_velocity_->init(effort_joint_interface_, nh_velocity))
-            return false;
-        joint_handles_.push_back(j.ctrl_position_->joint_);
-        joint_handles_.push_back(j.ctrl_velocity_->joint_);
-        joints_.push_back(j);
+      ROS_ASSERT(motion.second["position_config"][i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+      ROS_ASSERT(motion.second["velocity_config_"][i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+      ROS_ASSERT(motion.second["position_need_reverse"][i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+      ROS_ASSERT(motion.second["velocity_need_reverse"][i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+      m.position_config_.push_back(motion.second["position_config"][i]);
+      m.velocity_config_.push_back(motion.second["velocity_config_"][i]);
+      m.is_position_need_reverse_.push_back(motion.second["is_position_need_reverse"][i]);
+      m.is_velocity_need_reverse_.push_back(motion.second["is_velocity_need_reverse"][i]);
     }
-    for (const auto& motion : motions)
-    {
-        Motion m{ .motion_name_ = motion.first,
-                  .position_per_step_ = motion.second["position_per_step"],
-                  .velocity_max_speed_ = motion.second["velocity_max_speed"] };
-        for (int i = 0; i < motion.second["position_config"].size(); ++i)
-        {
-            ROS_ASSERT(motion.second["position_config"][i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
-            ROS_ASSERT(motion.second["velocity_config_"][i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
-            ROS_ASSERT(motion.second["position_need_reverse"][i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
-            ROS_ASSERT(motion.second["velocity_need_reverse"][i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
-            m.position_config_.push_back(motion.second["position_config"][i]);
-            m.velocity_config_.push_back(motion.second["velocity_config_"][i]);
-            m.position_need_reverse.push_back(motion.second["position_need_reverse"][i]);
-            m.velocity_need_reverse.push_back(motion.second["velocity_need_reverse"][i]);
-        }
-        motions_.push_back(m);
-    }
-    robot_state_handle_ = robot_hw->get<rm_control::RobotStateInterface>()->getHandle("robot_state");
-    cmd_multi_dof_sub_ = controller_nh.subscribe("command", 1, &Controller::commandCB, this);
-    return true;
-    }
+    motions_.push_back(m);
+  }
+  robot_state_handle_ = robot_hw->get<rm_control::RobotStateInterface>()->getHandle("robot_state");
+  cmd_multi_dof_sub_ = controller_nh.subscribe("command", 1, &Controller::commandCB, this);
+  return true;
+}
 
 void Controller::starting(const ros::Time& /*unused*/)
 {
-    state_ = VELOCITY;
-    state_changed_ = true;
+  state_ = VELOCITY;
+  state_changed_ = true;
 }
 
 void Controller::update(const ros::Time& time, const ros::Duration& period)
 {
-    cmd_multi_dof_ = *cmd_rt_buffer_.readFromRT();
-    if (state_ != cmd_multi_dof_.mode)
-    {
-        state_ = cmd_multi_dof_.mode;
-        state_changed_ = true;
-    }
-    switch (state_)
-    {
-        case VELOCITY:
-            velocity(time,period);
-            break;
-        case POSITION:
-            position(time,period);
-            break;
-    }
+  cmd_multi_dof_ = *cmd_rt_buffer_.readFromRT();
+  if (state_ != cmd_multi_dof_.mode)
+  {
+    state_ = cmd_multi_dof_.mode;
+    state_changed_ = true;
+  }
+  switch (state_)
+  {
+    case VELOCITY:
+      velocity(time, period);
+      break;
+    case POSITION:
+      position(time, period);
+      break;
+  }
 }
 
-void Controller::velocity(const ros::Time &time,const ros::Duration& period)
+void Controller::velocity(const ros::Time& time, const ros::Duration& period)
 {
-    if (state_changed_)
-    {  // on enter
-        state_changed_ = false;
-        ROS_INFO("[Multi_Fof] VELOCITY");
+  if (state_changed_)
+  {  // on enter
+    state_changed_ = false;
+    ROS_INFO("[Multi_Fof] VELOCITY");
+  }
+  std::vector<double> results{ (double)joints_.size() };
+  for (int i = 0; (int)i < joints_.size(); ++i)
+  {
+    for (int j = 0; (int)j < motions_.size(); ++j)
+    {
+      if (motions_[j].motion_name_ == cmd_multi_dof_.motion_name)
+      {
+        results[i] = judgeReverse(getDirectionValue(), motions_[j].is_velocity_need_reverse_[i]) *
+                     motions_[j].velocity_max_speed_ * motions_[j].velocity_config_[i];
+      }
     }
-    std::vector<double> results{(double)joints_.size()};
-    for (int i = 0; (int)i < joints_.size(); ++i) {
-
-            for (int j = 0; (int)j < motions_.size(); ++j) {
-                if (motions_[j].motion_name_ == cmd_multi_dof_.motion_name) {
-                    results[i] = judgeReverse(getDirectionValue(), motions_[j].is_velocity_need_reverse_[i]) *
-                                 motions_[j].velocity_max_speed_ * motions_[j].velocity_config_[i];
-                }
-            }
-    }
-    for (int i = 0; (int)i < joints_.size(); ++i) {
-        joints_[i].ctrl_velocity_->setCommand(results[i]);
-        joints_[i].ctrl_velocity_->update(time,period);
-    }
+  }
+  for (int i = 0; (int)i < joints_.size(); ++i)
+  {
+    joints_[i].ctrl_velocity_->setCommand(results[i]);
+    joints_[i].ctrl_velocity_->update(time, period);
+  }
 }
-void Controller::position(const ros::Time &time,const ros::Duration& period)
+void Controller::position(const ros::Time& time, const ros::Duration& period)
 {
-    if (state_changed_)
-    {  // on enter
-        state_changed_ = false;
-        ROS_INFO("[Multi_Fof] POSITION");
+  if (state_changed_)
+  {  // on enter
+    state_changed_ = false;
+    ROS_INFO("[Multi_Fof] POSITION");
+  }
+  std::vector<double> results{ (double)joints_.size() };
+  for (int i = 0; (int)i < joints_.size(); ++i)
+  {
+    for (int j = 0; (int)j < motions_.size(); ++j)
+    {
+      if (motions_[j].motion_name_ == cmd_multi_dof_.motion_name)
+      {
+        double total_step_value = judgeReverse(getDirectionValue(), motions_[j].is_position_need_reverse_[i]);
+        double step_num = total_step_value / motions_[j].position_per_step_;
+        results[i] = step_num * motions_[j].position_config_[i];
+      }
     }
-    std::vector<double> results{(double)joints_.size()};
-    for (int i = 0; (int)i < joints_.size(); ++i) {
-        for (int j = 0; (int)j < motions_.size(); ++j) {
-            if (motions_[j].motion_name_ == cmd_multi_dof_.motion_name)
-            {
-                double total_step_value = judgeReverse(getDirectionValue(),motions_[j].is_position_need_reverse_[i]);
-                double step_num = total_step_value / motions_[j].position_per_step_;
-                results[i] = step_num * motions_[j].position_config_[i];
-            }
-        }
-    }
-    for (int i = 0; (int)i < joints_.size(); ++i) {
-        joints_[i].ctrl_position_->setCommand(joints_[i].ctrl_position_->getPosition()+results[i]);
-        joints_[i].ctrl_position_->update(time,period);
-    }
+  }
+  for (int i = 0; (int)i < joints_.size(); ++i)
+  {
+    joints_[i].ctrl_position_->setCommand(joints_[i].ctrl_position_->getPosition() + results[i]);
+    joints_[i].ctrl_position_->update(time, period);
+  }
 }
-
 
 double Controller::getDirectionValue()
 {
-    double direction_value;
-    if (cmd_multi_dof_.motion_name == "x")
-        direction_value = cmd_multi_dof_.values.linear.x;
-    else if (cmd_multi_dof_.motion_name == "y")
-        direction_value = cmd_multi_dof_.values.linear.y;
-    else if (cmd_multi_dof_.motion_name == "z")
-        direction_value = cmd_multi_dof_.values.linear.z;
-    else if (cmd_multi_dof_.motion_name == "roll")
-        direction_value = cmd_multi_dof_.values.angular.x;
-    else if (cmd_multi_dof_.motion_name == "pitch")
-        direction_value = cmd_multi_dof_.values.angular.y;
-    else if (cmd_multi_dof_.motion_name == "yaw")
-        direction_value = cmd_multi_dof_.values.linear.z;
-    else
-        direction_value = 0;
-    return direction_value;
+  double direction_value;
+  if (cmd_multi_dof_.motion_name == "x")
+    direction_value = cmd_multi_dof_.values.linear.x;
+  else if (cmd_multi_dof_.motion_name == "y")
+    direction_value = cmd_multi_dof_.values.linear.y;
+  else if (cmd_multi_dof_.motion_name == "z")
+    direction_value = cmd_multi_dof_.values.linear.z;
+  else if (cmd_multi_dof_.motion_name == "roll")
+    direction_value = cmd_multi_dof_.values.angular.x;
+  else if (cmd_multi_dof_.motion_name == "pitch")
+    direction_value = cmd_multi_dof_.values.angular.y;
+  else if (cmd_multi_dof_.motion_name == "yaw")
+    direction_value = cmd_multi_dof_.values.linear.z;
+  else
+    direction_value = 0;
+  return direction_value;
 }
 
 double Controller::judgeReverse(double value, bool is_need_reverse)
 {
-    if (!is_need_reverse)
-        value = abs(value);
-    return value;
+  if (!is_need_reverse)
+    value = abs(value);
+  return value;
 }
-void Controller::commandCB(const rm_msgs::MultiDofCmdPtr &msg)
+void Controller::commandCB(const rm_msgs::MultiDofCmdPtr& msg)
 {
-    cmd_rt_buffer_.writeFromNonRT(*msg);
+  cmd_rt_buffer_.writeFromNonRT(*msg);
 }
-}
+}  // namespace multi_dof_controller
