505cca8e9285a450cd4c68316addbd58042125a4
xuncheng <205455603@qq.com>
2026-03-15 10:14:11 +0800

    Update the active suspension code.

--- rm_chassis_controllers/include/rm_chassis_controllers/active_suspension.h
+++ rm_chassis_controllers/include/rm_chassis_controllers/active_suspension.h
@@ -2,15 +2,25 @@
 // Created by xuncheng on 2025/11/29.
 //
 
-
 #include "rm_chassis_controllers/chassis_base.h"
 #include "rm_chassis_controllers/omni.h"
-#include <std_msgs/Bool.h>
 #include <effort_controllers/joint_position_controller.h>
 #include <rm_msgs/ChassisActiveSusCmd.h>
+#include <std_msgs/Bool.h>
+#include <rm_common/ros_utilities.h>
+#include <string>
+#include <Eigen/QR>
+#include <pluginlib/class_list_macros.hpp>
 
 namespace rm_chassis_controllers
 {
+    enum class State
+    {
+        DOWN = 0,
+        MID = 1,
+        UP = 2
+    };
+
     class ActiveSuspensionController : public OmniController
     {
     public:
@@ -25,38 +35,19 @@ namespace rm_chassis_controllers
         std::vector<hardware_interface::JointHandle> active_suspension_joint_handles_{};
 
         ros::Subscriber active_suspension_sub_;
+        double current_pos_{0.};
+        double target_pos_{0.};
+        double feedforward_offset{0.};
+        double feedforward_effort{0.};
+        double stretch_coff_A_{0.};
+        double stretch_coff_k_{0.};
+        double shrink_coff_A_{0.};
+        double shrink_coff_k_{0.};
+        double feedforward_effect_time_{0.};
+        double static_effort{0.} ;
 
-        double suspension_pos_;
-        double feed_forward_offset;
-        double feed_forward_effort;
-        double pos;
-		double suspension_pos_DOWN_;
-		double suspension_pos_MID_;
-		double suspension_pos_UP_;
-        double static_effort_DOWN_;
-        double static_effort_MID_;
-        double static_effort_UP_;
-        double stretch_coff_A_;
-        double stretch_coff_k_;
-        double shrink_coff_A_;
-        double shrink_coff_k_;
-        double feedforward_effect_time_;
-        double static_effort ;
-        int count_;
-        bool state_change_;
-        ros::Time state_change_time;
-
-
-      enum
-      {
-          DOWN,
-          MID,
-          UP
-      };
-
-        int state_ = DOWN;
-        int last_state_ = DOWN;
-        int check_state_ = DOWN;
-
+        ros::Time feedforward_timer;
+        State current_state_{State::DOWN};
+        State last_state_{State::DOWN};
   };
 }
--- rm_chassis_controllers/src/active_suspension.cpp
+++ rm_chassis_controllers/src/active_suspension.cpp
@@ -2,12 +2,6 @@
 // Created by qiayuan on 2022/7/29.
 //
 
-#include <string>
-#include <Eigen/QR>
-
-#include <rm_common/ros_utilities.h>
-#include <pluginlib/class_list_macros.hpp>
-
 #include "rm_chassis_controllers/active_suspension.h"
 
 namespace rm_chassis_controllers
@@ -20,19 +14,11 @@ namespace rm_chassis_controllers
 
         XmlRpc::XmlRpcValue suspension_legs;
         controller_nh.getParam("suspension_leg", suspension_legs);
-        feed_forward_offset = getParam(controller_nh, "feed_forward_offset", 0.);
         stretch_coff_A_ = getParam(controller_nh,"stretch_coff_A_",0.);
         stretch_coff_k_ = getParam(controller_nh,"stretch_coff_k_",0.);
         shrink_coff_A_ = getParam(controller_nh,"shrink_coff_A_",0.);
         shrink_coff_k_ = getParam(controller_nh,"shrink_coff_k_",0.);
-
-
-        suspension_pos_DOWN_ = getParam(controller_nh,"suspension_pos_DOWN_",0.);
-        suspension_pos_MID_ = getParam(controller_nh,"suspension_pos_MID_",0.);
-        suspension_pos_UP_ = getParam(controller_nh,"suspension_pos_UP_",0.);
-        static_effort_DOWN_ = getParam(controller_nh,"static_effort_DOWN_",0.);
-        static_effort_MID_ = getParam(controller_nh,"static_effort_MID_",0.);
-        static_effort_UP_ = getParam(controller_nh,"static_effort_UP_",0.);
+        feedforward_offset = getParam(controller_nh, "feed_forward_offset", 0.);
         feedforward_effect_time_ = getParam(controller_nh,"feedforward_effect_time_",0.);
 
         for (const auto& suspension_leg : suspension_legs)
@@ -50,79 +36,51 @@ namespace rm_chassis_controllers
     void ActiveSuspensionController::moveJoint(const ros::Time& time, const ros::Duration& period)
     {
         OmniController::moveJoint(time, period);
-        switch (state_)
+        switch (current_state_)
         {
-          case DOWN:
-            suspension_pos_ = suspension_pos_DOWN_;
+          case State::DOWN:
+            target_pos_ = -0.05;
             static_effort = -6.0;
             break;
-          case MID:
-            suspension_pos_ = suspension_pos_MID_;
+          case State::MID:
+            target_pos_ = 0.30;
+            static_effort = 9.0;
+            break;
+          case State::UP:
+            target_pos_ = 0.80;
             static_effort = 8.0;
             break;
-          case UP:
-            suspension_pos_ = suspension_pos_UP_;
-            static_effort = 0.0;
-            break;
         }
-        if (state_ != check_state_)
+        if (current_state_ != last_state_)
         {
-          count_ =0;
-          last_state_ =  check_state_;
+            last_state_ = current_state_;
+            feedforward_timer = ros::Time::now();
         }
-        if (last_state_ - state_ == -2 || ((last_state_ - state_ == - 1) && (state_ ==2)))
+        if ((ros::Time::now() - feedforward_timer).toSec() < feedforward_effect_time_)
         {
-          count_ ++;
-          if (count_ == 1)
-          {
-            state_change_time = ros::Time::now();
-          }
-          feed_forward_effort =  2 * (stretch_coff_A_ + stretch_coff_k_ * pos);
-        }
-        else if ( last_state_ - state_ == -1 && state_ == 1)
-        {
-          count_ ++;
-          if (count_ == 1)
-          {
-            state_change_time = ros::Time::now();
-          }
-          feed_forward_effort = stretch_coff_A_ - 45.0 * pos;
-        }
-        else if (last_state_ - state_ == 1 || last_state_ - state_ == 2)
-        {
-          count_ ++;
-          if (count_ == 1)
-          {
-            state_change_time = ros::Time::now();
-          }
-          feed_forward_effort = shrink_coff_A_ + shrink_coff_k_ * pos;
+          if (current_state_ == State::MID)
+              feedforward_effort = 1.2 * (stretch_coff_A_ - 45.0 * current_pos_);
+          else if (current_state_ == State::UP)
+              feedforward_effort =  2 * (stretch_coff_A_ + stretch_coff_k_ * current_pos_);
+          else if (current_state_ == State::DOWN)
+              feedforward_effort = shrink_coff_A_ + shrink_coff_k_ * current_pos_;
         }
         else
-        {
-          feed_forward_effort = 0.0;
-        }
+          feedforward_effort =  0.0;
 
         for (auto& joint : active_suspension_joints_)
         {
-            joint->setCommand(suspension_pos_);
+            joint->setCommand(target_pos_);
             joint->update(time, period);
-            pos = joint->getPosition();
+            current_pos_ = joint->getPosition();
             auto cmd = joint->joint_.getCommand();
-
-            cmd += feed_forward_effort + feed_forward_offset + static_effort;
+            cmd += feedforward_effort + feedforward_offset + static_effort;
             joint->joint_.setCommand(cmd);
         }
-        if (ros::Time::now().toSec() - state_change_time.toSec() > feedforward_effect_time_)
-        {
-          last_state_ = state_;
-          count_ = 0;
-        }
-        check_state_ = state_;
     }
     void ActiveSuspensionController::ActiveSuspensionCallBack(const rm_msgs::ChassisActiveSusCmd& msg)
     {
-      state_ = msg.mode;
+      current_state_ = static_cast<State>(msg.mode);
     }
-
 }  // namespace rm_chassis_controllers
 PLUGINLIB_EXPORT_CLASS(rm_chassis_controllers::ActiveSuspensionController, controller_interface::ControllerBase)
